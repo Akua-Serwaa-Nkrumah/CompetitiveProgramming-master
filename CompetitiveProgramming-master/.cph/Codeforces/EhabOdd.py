d0daebf387a2622f13e85af5b9a122bee06da017
Akua-Serwaa-Nkrumah <nkrumahakua2002gmail.com>
2024-01-02 10:39:13 +0000

    CompetitiveProgramming

--- CompetitiveProgramming-master/.cph/Codeforces/EhabOdd.py
+++ CompetitiveProgramming-master/.cph/Codeforces/EhabOdd.py
@@ -1,13 +1,14 @@
-def Odd():
-    n = int(input())
-    a = list(map(int,input().split()))
-    for i in range(n):
-        for j in range(n):
-            if (a[i]+a[j])%2 == 1 and a[i]>a[j]:
-                a[i],a[j] = a[j],a[i]
-            if all(a[i] <= a[i+1] for i in range(n-1)):
-                print(*a)
-                return
-            
-Odd()
+n = int(input())
+a = list(map(int,input().split()))
+odd, even = 0, 0
 
+for num in a:
+    if num%2 == 1:
+        odd += 1
+    else: 
+        even += 1
+
+if odd != 0 and even != 0:
+    a.sort()
+
+print(*a)
