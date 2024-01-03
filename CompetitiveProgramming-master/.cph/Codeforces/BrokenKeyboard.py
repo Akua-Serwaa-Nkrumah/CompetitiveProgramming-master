aa6bd683b1cecdc2ec4430e5e2a7bfc5bf24f57c
Akua-Serwaa-Nkrumah <nkrumahakua2002gmail.com>
2024-01-03 19:28:55 +0000

    Two pointers

--- CompetitiveProgramming-master/.cph/Codeforces/BrokenKeyboard.py
+++ CompetitiveProgramming-master/.cph/Codeforces/BrokenKeyboard.py
@@ -1,36 +1,21 @@
-# t = int(input())
-# for _ in range(t):
-#     s = input()
-
-#     faulty = []
-#     correct = set()
-#     if len(set(s))<3:
-#         print(0)
-#     else: 
-#         for i in range(len(s)-1):
-#             if all(s[i] == s[i+1]):
-#                 faulty.append(s[i])
-#         print (faulty)
-#         for letter in s:
-#             if letter not in faulty:
-#                 correct.add(letter)
-                
-#     print(correct)
 t = int(input())
 for _ in range(t):
     s = input()
 
     faulty = []
-    correct = set()
+    res = ''  
 
-    for i in range(len(s)-1):
-        if (s[i] == s[i+1]):
-            faulty.append(s[i])
-        
-    print(faulty)
+    for i in s:
+        if faulty and i == faulty[-1]:
+            faulty.pop()
+            res += "".join(faulty)
 
-    correct = set(s) - set(faulty)
-                
-    print(correct)
+            continue
+        faulty.append(i)
 
+    res += "".join(faulty)
+    res = sorted(set(res))
+    
+    print("".join(res))
+    
 
