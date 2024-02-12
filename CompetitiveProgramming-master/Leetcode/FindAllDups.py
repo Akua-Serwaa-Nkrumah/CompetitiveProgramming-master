56481f263698894c3bb62c705174f53bde788f30
Akua-Serwaa-Nkrumah <nkrumahakua2002gmail.com>
2024-02-12 17:46:52 +0000

    Cyclic Sort

---
+++ CompetitiveProgramming-master/Leetcode/FindAllDups.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findDuplicates(self, nums: [int]) -> [int]:
+        i = 0
+
+        while i < len(nums):
+            idx = nums[i] - 1
+
+            if idx != i:
+                if nums[idx] != nums[i]:
+                    nums[i], nums[idx] = nums[idx], nums[i]
+
+                else:
+                    i += 1
+
+            else:
+                i += 1
+        res = []
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                res.append(nums[i])
+
+        return res
