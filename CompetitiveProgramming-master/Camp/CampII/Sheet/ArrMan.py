1813192a961b933296e3720b0c2554c75e9888af
Akua-Serwaa-Nkrumah <nkrumahakua2002gmail.com>
2024-03-27 10:30:45 +0000

    Difference Array

---
+++ CompetitiveProgramming-master/Camp/CampII/Sheet/ArrMan.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    dif = [0]*(n+2)
+    for i in queries:
+        l,r, val = i[0],i[1],i[2]
+        dif[l] += val
+        dif[r+1] -= val
+    
+    for i in range(1,n+1):
+        dif[i] = dif[i-1] + dif[i]
+    return max(dif)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
